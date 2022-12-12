f1659fadc7b6c2d433317557a7f2b7a5a2fb84d4
krgirard33 <ken.girard@gmail.com>
2022-12-12 00:51:33 -0600

    Created equipmentList, addItem, removeItem, & ngFor list the equipment now

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipmentList: object[] = [
+    {name: "Habitat dome"},
+    {name: "Drones"},
+    {name: "Food containers"},
+    {name: "Oxygen tanks"},
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  addItem(itemNew: object) {
+    this.equipmentList.push({ name: itemNew })
+  }
+
+  removeItem(item: object){
+    let index = this.equipmentList.indexOf(item);
+    this.equipmentList.splice(index, 1)
+  }
 }
